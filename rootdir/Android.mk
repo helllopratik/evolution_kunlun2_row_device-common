3a3719d107ab95f492fb9f9e62451ee5d2392ba1
HelloPratik <38004222+pratik0007@users.noreply.github.com>
2022-09-23 20:40:42 +0530

    Add files via upload

---
+++ rootdir/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
